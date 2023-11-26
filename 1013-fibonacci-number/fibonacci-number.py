403c449e9906da63c2d35644044b9f07eeba0a34
Pratik Harsh <87686300+pratik295@users.noreply.github.com>
2023-11-26 10:55:30 +0530

    Time: 40 ms (62.85%) | Memory: 16.4 MB (5.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,7 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
+        dp={}
         def solve(n):
+
             if n<=1:
                 return n
-            return solve(n-1)+solve(n-2)
+            if n in dp:
+                return dp[n]
+            res= solve(n-1)+solve(n-2)
+            dp[n]=res
+            return res
         return solve(n)
